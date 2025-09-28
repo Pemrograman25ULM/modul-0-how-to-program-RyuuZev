f939d765b1fbb548919066ea876f297dba67682f
Pasya <tangleroad0101@gmail.com>
2025-09-29 03:52:10 +0800

    Update Prak 004

---
+++ C/PRAK004-2510817210026-MuhammadKurniawanPasya.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("#################################################\n");
+    printf("#                                               #\n");
+    printf("#          Muhammad Kurniawan Pasya             #\n");
+    printf("#               2510817210026                   #\n");
+    printf("#                                               #\n");
+    printf("#################################################\n");
+    getchar();
+    return 0;
+}
